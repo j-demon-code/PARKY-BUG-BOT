d8e79cbb0e0cf57965193e00e366564e91c25a28
j-demon-code <judekosi085@gmail.com>
2024-08-13 13:11:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
-global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
+global.ownernomer = "2348168437666"
+global.ownername = "Jude Wrld"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
